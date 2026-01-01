385451094df9b532ea440e745bf756cc54cecdc1
saman <saman@gmail.com>
2026-01-01 13:52:45 +0330

    iputt validation change

--- components/utils/validationInput/index.js
+++ components/utils/validationInput/index.js
@@ -1,8 +1,8 @@
 import * as Yup from 'yup'
 
 export const schemaValidInpArt = Yup.object().shape({
-  metaDiscription: Yup.string().required("متادیسکریپشن الزامی است").min(6, "متادیسکریپشن باید حداقل ۶ کاراکتر باشد").max(100, 'متادیسکریپشن نباید از 100 کارکتر بیشتر باشد'),
+  metaDiscription: Yup.string().required("متادیسکریپشن الزامی است").min(50, "متادیسکریپشن باید حداقل 50 کاراکتر باشد").max(200, 'متادیسکریپشن نباید از 200 کارکتر بیشتر باشد'),
   thumbnail: Yup.mixed().required("انتخاب فایل الزامی است"),
-  articleTitle: Yup.string().required("عنوان الزامی است").min(6, "عنوان باید حداقل ۶ کاراکتر باشد").max(50, 'عنوان نباید از 50 کارکتر بیشتر باشد'),
+  articleTitle: Yup.string().required("عنوان الزامی است").min(30, "عنوان باید حداقل 30 کاراکتر باشد").max(150, 'عنوان نباید از 150 کارکتر بیشتر باشد'),
   body: Yup.string('متن مقاله باید رشته باشد').required("متن مقاله الزامی است").min(500, "متن مقاله باید حداقل 500 کاراکتر باشد"),
 });
