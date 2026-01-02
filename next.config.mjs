0a4128a2cae21e622085c5c0bdf6b9fb43a1759f
saman <saman@gmail.com>
2026-01-02 14:04:37 +0330

    hostname next.config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'nivato.ir',
+                hostname: 'api.nivato.ir',
                 pathname: '/**',
             },
         ],
